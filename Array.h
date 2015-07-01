6c231d71f08464d0a427144f15f56343d2ce5e7a
minortones <minortones@yahoo.com>
2015-07-01 06:07:27 +0100

    bug fixes. additional vector-y functionality

--- Array.h
+++ Array.h
@@ -20,24 +20,30 @@
 /// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //////////////////////////////////////////////////////////////////////////
 
-#include <type_traits>
-#include <xmemory>
 
 #ifndef KS_ARRAY
 #define KS_ARRAY
 
+#include <type_traits>
+#include <xmemory>
 
 namespace ks {
 
+#if _DEBUG
+	#define KS_ASSERT( cond )		{ if( (cond)  == false ) __debugbreak(); }
+#else
+	#define KS_ASSERT( cond )
+#endif
+
 	namespace details
 	{
-		// use SFINAE to optimise trivially copyable types
+		// use SFINAE to optimize trivially copyable types
 		template <typename T = void>
 		using if_copyable_t = typename std::enable_if< std::is_trivially_copyable<T>::value, T>::type;
 
 		template <typename T = void>
 		using if_non_copyable_t = typename std::enable_if< !std::is_trivially_copyable<T>::value, T>::type;
-		
+
 		template<typename T>
 		void copy_into( if_copyable_t<T> *& pDest, const T* pSource, const size_t pSize )
 		{
@@ -45,7 +51,7 @@ namespace ks {
 		}
 
 		template<typename T>
-		void copy_into( if_non_copyable_t<T> *& pDest, const T* pSource, const size_t pSize )
+		void copy_into( if_non_copyable_t<T> *& pDest, T* pSource, const size_t pSize )
 		{
 			size_t i = 0;
 			switch ((pSize - i) & 7)	// Duff's device
@@ -53,14 +59,14 @@ namespace ks {
 			case 0:
 				while (i != pSize)
 				{
-					pDest[ i ] = pSource[ i++ ];
-			case 7: pDest[ i ] = pSource[ i++ ];
-			case 6: pDest[ i ] = pSource[ i++ ];
-			case 5: pDest[ i ] = pSource[ i++ ];
-			case 4: pDest[ i ] = pSource[ i++ ];
-			case 3: pDest[ i ] = pSource[ i++ ];
-			case 2: pDest[ i ] = pSource[ i++ ];
-			case 1: pDest[ i ] = pSource[ i++ ];
+					pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 7: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 6: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 5: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 4: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 3: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 2: pDest[ i ] = std::move( pSource[ i ] ); ++i;
+			case 1: pDest[ i ] = std::move( pSource[ i ] ); ++i;
 				}
 			}
 		}
@@ -78,20 +84,20 @@ namespace ks {
 			for ( size_t i = 0; i < pSize; ++i )
 				pAllocator.construct( pDest + i, T() );
 		}
-		
+
 
 		template<typename T, class TAllocator>
-		void deallocate(if_copyable_t<T> *& pBegin, const size_t pCapacity, TAllocator& pAllocator)
+		void deallocate( if_copyable_t<T> *& pBegin, const size_t pCapacity, TAllocator& pAllocator)
 		{
 			pAllocator.deallocate(pBegin, pCapacity);
 		}
 
 		template<typename T, class TAllocator>
-		void deallocate( if_non_copyable_t<T> *& pBegin, const size_t pCapacity, TAllocator& pAllocator)
+		void deallocate( if_non_copyable_t<T>*& pBegin, const size_t pCapacity, TAllocator& pAllocator)
 		{
 			for ( size_t i = 0; i < pCapacity; ++i )
 				pAllocator.destroy( pBegin + i );
-			
+
 			pAllocator.deallocate(pBegin, pCapacity);
 		}
 
@@ -117,34 +123,97 @@ namespace ks {
 		}
 	}
 
+	template<typename T>
+	struct ReverseIterator
+	{
+		ReverseIterator() : mBegin(nullptr), mEnd(nullptr)
+		{}
+		ReverseIterator( T* pBegin, T* pEnd ) : mBegin(nullptr), mEnd(nullptr)
+		{
+			if(pBegin && pEnd > pBegin)
+			{
+				mBegin	= pBegin;
+				mEnd	= pEnd - 1;
+			}
+		}
+
+		bool operator==( const ReverseIterator& pOther ) const	{ return mBegin == pOther.mBegin && mEnd == pOther.mEnd; }
+		bool operator!=( const ReverseIterator& pOther ) const	{ return !operator==(pOther); }
+
+		T& operator->()					{ return *mEnd; }
+		const T& operator->() const		{ return *mEnd; }
+		T& operator*()					{ return *mEnd; }
+		const T& operator*() const		{ return *mEnd; }
+
+		ReverseIterator& operator++()
+		{
+			if (mEnd <= mBegin)		// TODO: optimize away in Release config
+			{
+				mEnd = mBegin = nullptr;
+			}
+			else
+			{
+				--mEnd;
+			}
+			return *this;
+		}
+
+		ReverseIterator& operator++(int)
+		{
+			return operator++();
+		}
+
+		ReverseIterator& operator+(const int pCount)
+		{
+			mEnd = mEnd - pCount;
+#if _DEBUG
+			if ( mEnd < mBegin || mEnd > (mEnd + pCount) )
+			{
+				KS_ASSERT( 0 && "Bounds exceeded" );
+				mEnd = mBegin;
+			}
+#endif
+			return *this;
+		}
+
+	private:
+		T* mBegin;
+		T* mEnd;
+	};
+
 
 	template<typename T, class TAllocator = std::allocator<T> >
 	class Array 
 	{
-#define KS_ARRAY_MOVE_SEMANTICS_ONLY	1
+#define KS_ARRAY_MOVE_SEMANTICS_ONLY	1		// Before turning this off, have you considered using std::vector? :P
 
 #if _DEBUG
-	#define CHECK_OUT_OF_BOUNDS( x )	if( (x) >= _capacity ) { __debugbreak(); /*KS_ASSERT( "Array out of bounds detected" );*/ }
+#define CHECK_OUT_OF_BOUNDS( x )	if( (x) >= _capacity ) { KS_ASSERT( 0 && "Array out of bounds detected" ); }
 #else
-	#define CHECK_OUT_OF_BOUNDS( x )
+#define CHECK_OUT_OF_BOUNDS( x )
 #endif
 
-public:
-	typedef typename T*			iterator;
-	typedef typename const T*	const_iterator;
+	public:
+		typedef typename T*					iterator;
+		typedef typename const T*			const_iterator;
 
-		size_t size() const		{ return _size;			}
-		bool any() const 		{ return _size != 0;	}
-		bool empty() const		{ return _size == 0;	}
+		typedef typename ReverseIterator<T>	reverse_iterator;
 
-		T* begin() 				{ return _begin;			}
-		const T* begin() const	{ return _begin;			}
-		T* end() 				{ return _begin + _size;	}
-		const T* end() const	{ return _begin + _size;	}
+		size_t size() const				{ return _size;			}
+		bool any() const 				{ return _size != 0;	}
+		bool empty() const				{ return _size == 0;	}
+
+		iterator begin() 				{ return _begin;			}
+		const_iterator begin() const	{ return _begin;			}
+		iterator end() 					{ return _begin + _size;	}
+		const_iterator end() const		{ return _begin + _size;	}
+
+		reverse_iterator rbegin() const	{ return reverse_iterator(_begin, _begin + _size); }
+		reverse_iterator rend() const	{ return reverse_iterator(); }
 
 		T& front() 				{ CHECK_OUT_OF_BOUNDS(0); return _begin[0];			}
 		const T& front() const	{ CHECK_OUT_OF_BOUNDS(0); return _begin[0];			}
-		T& back() 				{ CHECK_OUT_OF_BOUNDS(_size - 1); return _begin[_size-1];	}
+		T& back() 				{ CHECK_OUT_OF_BOUNDS(_size - 1); return _begin[_size - 1];	}
 		const T& back() const	{ CHECK_OUT_OF_BOUNDS(_size - 1); return _begin[_size - 1]; }
 
 		void pop_back()			{ CHECK_OUT_OF_BOUNDS(_size - 1); --_size; }
@@ -185,7 +254,7 @@ public:
 			if (new_capacity > 0)
 			{
 				details::allocate<T>( new_data, new_capacity, _allocator );
-				details::copy_into( new_data, _begin, _size );		// TODO: move semantics if_non_trivially_copyable?
+				details::copy_into( new_data, _begin, _size );
 			}
 			details::deallocate<T>(_begin, _capacity, _allocator);
 			_begin		= new_data;
@@ -194,7 +263,7 @@ public:
 
 		void grow(size_t min_capacity = 0)
 		{
-			size_t new_capacity = _capacity * 2 + 8;
+			size_t new_capacity = _capacity + (_capacity >> 3) + 8;
 			if (new_capacity < min_capacity)
 				new_capacity = min_capacity;
 			set_capacity(new_capacity);
@@ -212,6 +281,38 @@ public:
 			_begin[_size++] = std::move(item);
 		}
 
+		iterator erase( iterator item )
+		{
+			const size_t new_size = _size - 1;
+			CHECK_OUT_OF_BOUNDS( new_size );
+			iterator next = item;
+
+			while (item != end())
+			{
+				*item = *(item + 1);
+				++item;
+			}
+			resize( new_size );
+
+			return next;
+		}
+
+		void insert( iterator pPos, const T& pValue )
+		{
+			const size_t diff = pPos - begin();
+			resize( _size + 1 );
+			CHECK_OUT_OF_BOUNDS( diff );
+			
+			pPos = begin() + diff;
+
+			for ( auto i = end() - 1; i > pPos; --i )
+			{
+				*i = std::move( *(i - 1) );
+			}
+
+			*pPos	= std::move(T(pValue));
+		}
+
 
 		Array() : _begin(nullptr), _size(0), _capacity(0), _allocator( TAllocator() )
 		{}
@@ -225,27 +326,6 @@ public:
 		{
 			details::deallocate<T>(_begin, _capacity, _allocator);
 		}
-
-#if !KS_ARRAY_MOVE_SEMANTICS_ONLY
-		Array(const Array &other) : _allocator(other._allocator), _size(0), _capacity(0), _begin(nullptr)
-		{
-			const size_t n = other._size;
-			set_capacity(n);
-			details::copy_into(_begin, other.begin_, sizeof(T) * n );
-			_size = n;
-		}
-
-		Array &operator=(const Array &other)
-		{
-			const size_t n = other._size;
-			resize(n);
-			details::copy_into(_begin, other.begin_, sizeof(T)*n );
-			return *this;
-		}
-#else
-		Array(const Array &other) = delete;
-		Array &operator=(const Array &other) = delete;
-#endif
 
 		Array(Array &&other)
 			: _begin(other._begin)
@@ -276,10 +356,41 @@ public:
 			}
 			return *this;
 		}
-
 		T & operator[](size_t i)				{ CHECK_OUT_OF_BOUNDS( i ); return _begin[i]; }
-
 		const T & operator[](size_t i) const	{ CHECK_OUT_OF_BOUNDS( i ); return _begin[i]; }
+
+		T & at(size_t i)						{ return operator[](i); }
+		const T & at(size_t i) const			{ return operator[](i); }
+
+		void explicit_copy( Array & pDest, const Array & pSource)
+		{
+			const size_t n = pSource._size;
+			pDest.resize(n);
+			details::copy_into( pDest._begin, pSource._begin, n );
+		}
+
+#if !KS_ARRAY_MOVE_SEMANTICS_ONLY
+
+		Array(const Array &other) : _allocator(other._allocator), _size(0), _capacity(0), _begin(nullptr)
+		{
+			set_capacity( other._capacity );
+			resize( other._size );
+			details::copy_into(_begin, other._begin, other._size );
+		}
+
+
+		Array &operator=(const Array &other)
+		{
+			const size_t n = other._size;
+			resize(n);
+			details::copy_into(_begin, other._begin, n );
+			return *this;
+		}
+#else
+	private:
+		Array(const Array &other);
+		Array &operator=(const Array &other);
+#endif
 
 
 	private:
@@ -290,7 +401,7 @@ public:
 	};
 
 
-}	//namespace ks
+}	//namespace KS
 
 
 #endif	// KS_ARRAY
